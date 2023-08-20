7237f9861eb699fdfa05465d50f4c34586e099eb
Felix Martinez Casadiego <felixmartinezcasadiego@MacBook-Pro-de-Felix.local>
2023-08-20 22:25:06 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  GoogleLogin,
+  GoogleOAuthProvider,
+  googleLogout,
+} from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+
+const clienId = process.env.REACT_APP_CLIENT_ID as string;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleOAuthProvider clientId={clienId}>
+        <div>
+          <h1>Google Calendar API</h1>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                if (credentialResponse.credential) {
+                  var decoded = jwt_decode(credentialResponse.credential);
+                }
+                console.log(decoded);
+                console.log("credentialResponse ----> ", credentialResponse);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+          <button onClick={() => googleLogout()} style={{ marginTop: "20px" }}>
+            <h3>Logout</h3>
+          </button>
+        </div>
+      </GoogleOAuthProvider>
     </div>
   );
 }
